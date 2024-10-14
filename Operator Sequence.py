4302bdffddeb3ba70eec7d7d084238ffd2c34171
toxic123MMM <aashish02@gmail.com>
2024-10-14 17:30:11 +0400

    Add files via upload

---
+++ Operator Sequence.py
@@ -0,0 +1,14 @@
+a=1
+b=2
+c=3
+d=4
+e=0
+e= (a+b)*c/d
+print("the value of(a+b)*c/d is: ",e)
+
+name ="Manansh"
+age =11
+if name == "Manansh" or  name == "khianna" and age<=5:
+    print("You got it right")
+else:
+    print("ohoh... You got it Wrong")
